65ca3df9e514e5b6a8ff65dec1f2d2132c752eaa
Dmitriy Korbut <d.korbut@vk.team>
2023-03-05 16:49:03 +0300

    Fix uniq function

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,18 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (!arr) {
+    return [];
+  }
+  const answer = [];
+  const answerMap = {};
 
+  for (const element of arr) {
+    if (!answerMap[element]) {
+      answerMap[element] = true;
+      answer.push(element);
+    }
+  }
+
+  return answer;
 }
